61dccb5bfb500ee027f9aff10c6c3483649fef04
Vinicius Martins <martinsrvinicius@gmail.com>
2024-05-07 08:56:32 +0100

    Not found route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import ClientHome from './routes/ClientHome';
 import ProductDetails from './routes/ClientHome/ProductDetails';
 import Catalog from './routes/ClientHome/Catalog';
@@ -13,6 +13,7 @@ export default function App() {
           <Route path="catalog" element={<Catalog />} />
           <Route path="product-details/:productId" element={<ProductDetails />} />
         </Route>
+        <Route path="*" element={<Navigate to="/"/>} />
       </Routes>
     </BrowserRouter>
   );
